d4228ab5455358c5f6013fd394a4d17629241802
PabloGJBS <plabo.jbs@gmail.com>
2023-11-28 17:26:15 -0300

    Adicionando Telas de História

---
+++ scripts/historia2.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# Aqui começa o jogo
+#func _on_button_skip_pressed():
+	#pass # Replace with function body.
+
+
+func _on_button_skip_pressed():
+	pass # Replace with function body.
