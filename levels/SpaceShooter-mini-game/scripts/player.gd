aeca79ac3b48c6f024b5c412cc9a697387b19b1d
CarolCoCe <anaipva@gmail.com>
2023-12-05 17:54:35 -0300

    colocando mais coisas do space shooter

--- levels/SpaceShooter-mini-game/scripts/player.gd
+++ levels/SpaceShooter-mini-game/scripts/player.gd
@@ -1,28 +1,17 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	var direction = Input.get_axis("ui_up", "ui_down")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.y = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
