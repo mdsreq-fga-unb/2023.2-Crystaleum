dbc9fbd4944aee085956fb868ceac1dae552de0c
CarolCoCe <anaipva@gmail.com>
2023-12-06 00:13:13 -0300

    Jogo da nave com o player e o laser ok, botoes tb #44

---
+++ levels/SpaceShooter-mini-game/scripts/laser.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var SPEED = 60
+
+func  _physics_process(delta):
+	global_position.x += SPEED * delta
+	
+	
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
