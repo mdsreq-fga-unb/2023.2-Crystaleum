ba83a2232fe8bfd540ffd4c167cfff8f37a898d4
PabloGJBS <plabo.jbs@gmail.com>
2023-12-08 22:27:03 -0300

    troca de fontes

--- resources/mini-caderno-resource/mini-caderno.gd
+++ resources/mini-caderno-resource/mini-caderno.gd
@@ -15,17 +15,17 @@ func get_atividade ():
 	
 	match mini_fase:
 		0:
-			atividade = "Falar com a mamãe sobre o vovô e descobrir que baú é aquele"
+			atividade = "falar com a mamãe sobre o vovô e descobrir que baú é aquele"
 		1:
-			atividade = "Ir pra casa abandonada do vovô e descobrir algo interessante"
+			atividade = "ir pra casa abandonada do vovô e descobrir algo interessante"
 		2:	
-			atividade = "Ir até a pracinha e entrar em uma facção legal"
+			atividade = "ir até a pracinha e entrar em uma facção legal"
 		3:	
-			atividade = "Ir para a sede da sua facção e falar com o comandante estelar"
+			atividade = "ir para a sede da sua facção e falar com o comandante estelar"
 		4: 
-			atividade = "Ir para o farol e pescar 3 oniguiris para ganhar a chave dos piratas espaciais"
+			atividade = "ir para o farol e pescar 3 oniguiris para ganhar a chave dos piratas espaciais"
 		5:	
-			atividade = "Voltar para casa e abrir o baú deixado pelo vovô"
+			atividade = "voltar para casa e abrir o baú deixado pelo vovô"
 		6:	
-			atividade = "Achei o crystal!!!!"
+			atividade = "achei o crystal!!!!"
 	return atividade
