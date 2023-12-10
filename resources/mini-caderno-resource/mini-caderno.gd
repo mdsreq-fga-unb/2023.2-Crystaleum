454f225bb0876682d612b1c5b9996d921ed41e4e
Artur <artujack28@gmail.com>
2023-12-10 18:53:59 -0300

    Fix quiz

--- resources/mini-caderno-resource/mini-caderno.gd
+++ resources/mini-caderno-resource/mini-caderno.gd
@@ -15,17 +15,17 @@ func get_atividade ():
 	
 	match mini_fase:
 		0:
-			atividade = "falar com a mamãe sobre o vovô e descobrir que baú é aquele"
+			atividade = "fale com a mamãe sobre o vovô e descubra que baú é aquele"
 		1:
-			atividade = "ir pra casa abandonada do vovô e descobrir algo interessante"
+			atividade = "ir pra casa abandonada do vovô e descubra algo interessante"
 		2:	
-			atividade = "ir até a pracinha e entrar em uma facção legal"
+			atividade = "ir até a pracinha e entre em uma facção legal"
 		3:	
-			atividade = "ir para a sede da sua facção e falar com o comandante estelar"
+			atividade = "ir para a porta da sede do Comando Estelar e fale com o Comandante"
 		4: 
-			atividade = "ir para o farol e pescar 3 oniguiris para ganhar a chave dos piratas espaciais"
+			atividade = "ir para o farol e pesque 3 oniguiris para ganhar a chave dos piratas espaciais"
 		5:	
-			atividade = "voltar para casa e abrir o baú deixado pelo vovô"
+			atividade = "volte para casa e abra o baú deixado pelo vovô"
 		6:	
 			atividade = "achei o crystal!!!!"
 	return atividade
