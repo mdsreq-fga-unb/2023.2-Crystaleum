9c245261cc124c18ecf3d0d64183bc94312e1e27
CarolCoCe <anaipva@gmail.com>
2023-12-10 14:57:40 -0300

    nome universo quiz e preload bau

--- scenes/bau.gd
+++ scenes/bau.gd
@@ -7,6 +7,7 @@ var is_character_colliding = false
 # Chamado quando o nó entra na árvore de cena pela primeira vez.
 func _ready():
 	animation_player.play("idle")  # Inicia com a animação de 'idle'
+	preload("res://levels/Casa-Crys/end_key.tscn")
 # Chamado a cada quadro. 'delta' é o tempo decorrido desde o quadro anterior.
 
 func _on_Bau_body_entered(body):
