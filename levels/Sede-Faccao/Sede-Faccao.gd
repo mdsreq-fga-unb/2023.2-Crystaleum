cd7e8b6d5b8ee607470fbe4291b95cffae3e7451
CarolCoCe <anaipva@gmail.com>
2023-12-11 15:13:32 -0300

    tasks da [BUGS] - ULTIMO GÁAAAAAAAAAAS  #66

--- levels/Sede-Faccao/Sede-Faccao.gd
+++ levels/Sede-Faccao/Sede-Faccao.gd
@@ -12,6 +12,7 @@ func _process(delta):
 
 
 func _on_button_pressed():
+	if Global.talkcomandante1:
 		get_tree().change_scene_to_file("res://levels/SpaceShooter-mini-game/scenes/instructions.tscn")
 
 
