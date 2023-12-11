1447771797ab8bc27fe41f3f795001d9e8653d65
suzane duarte <ssuzane9@hotmail.com>
2023-12-11 04:06:43 -0300

    alterando diálogos mini-caderno #66

--- resources/mini-caderno-resource/mini-caderno.gd
+++ resources/mini-caderno-resource/mini-caderno.gd
@@ -15,17 +15,17 @@ func get_atividade ():
 	
 	match mini_fase:
 		0:
-			atividade = "fale com a mamãe sobre o vovô e descubra que baú é aquele"
+			atividade = "converse com sua mãe sobre o vovô e os mistérios que o cercam"
 		1:
-			atividade = "ir pra casa abandonada do vovô e descubra algo interessante"
+			atividade = "vá para casa abandonada do vovô e explore todo aquele lugar"
 		2:	
-			atividade = "ir até a pracinha e entre em uma facção legal"
+			atividade = "vá para os três prédios do mapa e converse com os líderes de facção"
 		3:	
-			atividade = "Entrar na porta do Comando Estelar e fale com o Comandante"
+			atividade = "entre pela porta do Comando Estelar e fale com o Comandante na TV"
 		4: 
-			atividade = "ir para o farol e falar com os piratas espaciais"
+			atividade = "vá para o farol e converse com os piratas espaciais"
 		5:	
-			atividade = "volte para casa e abra o baú deixado pelo vovô"
+			atividade = "volte para sua casa e abra o baú deixado pelo vovô"
 		6:	
 			atividade = "achei o crystal!!!!"
 	return atividade
