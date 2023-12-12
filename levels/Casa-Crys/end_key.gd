5462a4568beeb42423462fbb14c98e41d99f003d
CarolCoCe <anaipva@gmail.com>
2023-12-12 01:05:35 -0300

    Correcao final

--- levels/Casa-Crys/end_key.gd
+++ levels/Casa-Crys/end_key.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
 
-
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://levels/Casa-Crys/casa.tscn")
+func _on_button_skip_pressed():
+	get_tree().change_scene_to_file("res://scenes/moonwalkers.tscn")
